b904030810f471751c1651705bc6aa8716b762f7
Your Name <you@example.com>
2022-12-05 02:17:23 -0800

    deploy1

--- utils/config.py
+++ utils/config.py
@@ -1,12 +1,12 @@
 class config:
-    # host = "containers-us-west-133.railway.app"
-    # user ="root"
-    # password = "e01yHEJYCDNW9GeJWv3g"
-    # database = "railway"
-    # port = 7930
-
-    host = "localhost"
+    host = "containers-us-west-133.railway.app"
     user ="root"
-    password = ""
-    database = "bot_db"
-    port = 3306
+    password = "e01yHEJYCDNW9GeJWv3g"
+    database = "railway"
+    port = 7930
+
+    # host = "localhost"
+    # user ="root"
+    # password = ""
+    # database = "bot_db"
+    # port = 3306
