efa192de1eaad241e359d2f6fde964edd3f02252
memde237 <dimitrimende@gmail.com>
2023-01-07 06:56:45 +0100

    deploy

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,12 @@
 class config:
-    host = "localhost"
+    # host = "localhost"
+    # user ="root"
+    # password = "root"
+    # database = "bot_db"
+    # port = 3306
+
+    host = "containers-us-west-133.railway.app"
     user ="root"
-    password = "root"
-    database = "bot_db"
-    port = 3306
+    password = "PyX1zXdedWA0zJ2ZqHt1"
+    database = "railway"
+    port = 7930
